58f0812289c78980c4106bee4f23b596a23039c2
Nilofarn <nilofar.nawabi@tekschool.us>
2023-03-01 13:27:34 -0500

    Capstone project code

--- src/test/java/tek/sdet/framework/runner/TestRunner.java
+++ src/test/java/tek/sdet/framework/runner/TestRunner.java
@@ -8,7 +8,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		tags ="@dryRun",
+		tags ="@Regresion",
         features = "classpath:features", 
         glue = "tek.sdet.framework",
         dryRun = false,
